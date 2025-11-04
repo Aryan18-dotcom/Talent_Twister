ac5c31882207d1a0d4b3f816c904cae2777b539a
Aryan18-dotcom <aryanchheda18@gmail.com>
2025-11-04 14:05:43 +0530

    Configure PostgreSQL database settings
    Added configuration for PostgreSQL using dj_database_url.

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from decouple import config, Csv # If using python-decouple
 import dj_database_url # Add this import for PostgreSQL on Render
+dj_database_url.config()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
