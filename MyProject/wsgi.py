6e15a696e888d0d79cd7232ce781cd679b400067
Aryan18-dotcom <aryanchheda18@gmail.com>
2025-11-04 13:07:38 +0530

    Refactor comments in wsgi.py to use hash symbols
    Updated comments to use hash symbols for clarity.

--- MyProject/wsgi.py
+++ MyProject/wsgi.py
@@ -1,16 +1,21 @@
-"""
-WSGI config for MyProject project.
+# """
+# WSGI config for MyProject project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+# """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProject.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProject.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+# Temporary code to auto-create superuser (remove after 1st deployment)
+try:
+    from Myapp.create_admin import *
+except Exception as e:
+    print("Superuser creation skipped:", e)
